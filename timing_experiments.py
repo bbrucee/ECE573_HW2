250a40033bb2367ae5615c55b14061e3fe293cac
Brendan <abae97@gmail.com>
2019-02-24 18:12:52 -0500

    gonna take a break then do timing and mains

---
+++ timing_experiments.py
@@ -0,0 +1,9 @@
+from Q1.Q1 import insertion_sort, shell7, shell3, shell_sort
+from Q2.Q2 import kd_merge, kd_mergesort, kendalltau
+from Q3.Q3 import q3_dataset_creation, bubblesort
+from Q4.Q4 import merge, mergesort_insertion_cutoff, iterative_mergesort, recursive_mergesort
+from Q5.Q5 import median_of_three, partition, quicksort, quicksort_insertion_cutoff
+from glob import glob
+import pandas as pd
+import os
+import timeit
